9ae6cb3397e19b2894f1bf6b14a490cc8e065cab
stiffmeister923 <106596734+stiffmeister923@users.noreply.github.com>
2025-04-17 20:35:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "LESSSSSGAWWWW, fastapi backend  naten guys deployed to sa railway"}
+
+@app.get("/JC")
+async def root():
+    return {"Eto sample jc"}
